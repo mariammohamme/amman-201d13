c3221ff739af77e40c12b3667a3d58943b980bd3
mariammohamme <48714020+mariammohamme@users.noreply.github.com>
2021-01-03 13:31:33 +0200

    Update app.js

--- class-01/demo/js/app.js
+++ class-01/demo/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var userName = prompt('What is your name?');
+/*var userName = prompt('What is your name?');
 console.log('userName value', userName);
 
 var favManufacturer = prompt('What is your favorite car manufacturer?');
@@ -8,5 +8,21 @@ console.log('favManufacturer', favManufacturer);
 
 var carModel = prompt('What is your favorite car model?');
 console.log('carModel', carModel);
+*/
+// MY WORK 
+var userOld = prompt('how old are you?');
+consol.log('Your age is :', userOld);
 
-alert('Hello '+userName+' , you like cars manufactured by '+ favManufacturer + ' specially '+ carModel);
+var userWork = prompt('Whate is your job??');
+consol.log('Your job is :', userWork);
+
+var userExperince = prompt('how many years experiance?');
+consol.log('You have :'+ userExperiance + 'years');
+
+var userCompany = prompt('whate is company name?');
+consol.log('Your company is :'+ userCompany );
+
+
+// alert('Hello '+userName+' , you like cars manufactured by '+ favManufacturer + ' specially '+ carModel);
+alert('age is: '+userOld+' , you like work as : '+ userWork + ' , you expert on job you have : '+ userExperince+' , you work in '+userCompany);
+
